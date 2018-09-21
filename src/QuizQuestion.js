a73946a3fa14a75c7f99746a3c948d5e17aae230
Kristian Svane Rømer (KVR) <KVR@kmd.dk>
2018-09-21 12:35:22 +0200

    test

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,20 +3,18 @@ import React, {Component } from 'react'
 class QuizQuestion extends Component{
   render(){
     return (
-    <main>
-      <section>
-        <p>
-        {this.props.quiz_questions.instruction_text}
-        </p>
-      </section>
-      <section>
-        <ul>
-        <li>{this.props.quiz_questions.answer_options[0]}</li>
-        </ul>
-      </section>
-    </main>
-  )
-    }
+      <main>
+          <section>
+            <p>
+            </p>
+          </section>
+          <section className="buttons">
+            <ul>
+            </ul>
+          </section>
+        </main>
+    )
+  }
 }
 
 export default QuizQuestion
